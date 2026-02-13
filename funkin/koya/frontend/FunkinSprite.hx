9d58da736f92f20d5847fdb3919a368fc77b4c8c
Macohi <sinconsistencia@gmail.com>
2026-02-12 18:37:07 -0600

    MSprite gets parseAnimOffset

--- funkin/koya/frontend/FunkinSprite.hx
+++ funkin/koya/frontend/FunkinSprite.hx
@@ -5,19 +5,4 @@ import macohi.overrides.MSprite;
 class FunkinSprite extends MSprite
 {
 	public var cameraOffsets:Array<Float> = [0, 0];
-
-	public function parseAnimationOffsetFile(offsetFile:Array<String>)
-	{
-		for (line in offsetFile)
-		{
-			var splitLine = line.split(' ');
-
-			var anim = splitLine[0] ?? null;
-			var x = splitLine[1] ?? '0';
-			var y = splitLine[2] ?? '0';
-
-			if (anim != null)
-				addOffset(anim, Std.parseFloat(x), Std.parseFloat(y));
-		}
-	}
 }
