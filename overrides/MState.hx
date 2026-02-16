03a4e7e20ca465303c76330ca8bc213d9a7d756f
Macohi <sinconsistencia@gmail.com>
2026-02-16 09:12:26 -0600

    switchState functions

--- overrides/MState.hx
+++ overrides/MState.hx
@@ -1,5 +1,13 @@
 package macohi.overrides;
 
+import flixel.FlxG;
 import flixel.FlxState;
+import flixel.util.typeLimit.NextState;
 
-class MState extends FlxState {}
+class MState extends FlxState
+{
+	public function switchState(state:NextState)
+	{
+		FlxG.switchState(state);
+	}
+}
--- overrides/MSubState.hx
+++ overrides/MSubState.hx
@@ -1,5 +1,13 @@
 package macohi.overrides;
 
+import flixel.FlxG;
 import flixel.FlxSubState;
+import flixel.util.typeLimit.NextState;
 
-class MSubState extends FlxSubState {}
+class MSubState extends FlxSubState
+{
+	public function switchState(state:NextState)
+	{
+		FlxG.switchState(state);
+	}
+}
