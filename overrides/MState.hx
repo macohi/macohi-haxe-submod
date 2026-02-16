e6a9a1ebf8513c3fd1f75e3cb10f840b96d87515
Macohi <sinconsistencia@gmail.com>
2026-02-16 12:24:30 -0600

    watermark texts

--- overrides/MState.hx
+++ overrides/MState.hx
@@ -2,6 +2,7 @@ package macohi.overrides;
 
 import flixel.FlxG;
 import flixel.FlxState;
+import flixel.util.FlxColor;
 import flixel.util.typeLimit.NextState;
 
 class MState extends FlxState
@@ -9,5 +10,22 @@ class MState extends FlxState
 	public function switchState(state:NextState)
 	{
 		FlxG.switchState(state);
+	}
+
+	public var leftWatermark:MText = new MText(10, 10, FlxG.width, 'left watermark', 8);
+	public var rightWatermark:MText = new MText(10, 10, FlxG.width, 'right watermark', 8);
+
+	override function create() {
+		super.create();
+
+		leftWatermark.alignment = LEFT;
+		rightWatermark.alignment = RIGHT;
+
+		for (watermark in [leftWatermark, rightWatermark])
+		{
+			watermark.setBorderStyle(OUTLINE, FlxColor.BLACK, 3);
+			watermark.visible = false;
+			add(watermark);
+		}
 	}
 }
