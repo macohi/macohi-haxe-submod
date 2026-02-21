aaeadfb18033ba38183df6a69c2f3a65d1bf7776
Macohi <sinconsistencia@gmail.com>
2026-02-21 09:45:21 -0600

    gbggbgbbvb

--- backend/DefineManager.hx
+++ backend/DefineManager.hx
@@ -11,13 +11,13 @@ class DefineManager
 	public static macro function getDefine(define:ExprOf<String>)
 		return macro $v{Context.definedValue(key)};
 
-	public static function isDefineDefined(define:ExprOf<String>):Bool
+	public static macro function isDefineDefined(define:ExprOf<String>):Bool
 		return getDefine(define) != null;
 
-	public static function doesDefineHaveValue(define:ExprOf<String>):Bool
+	public static macro function doesDefineHaveValue(define:ExprOf<String>):Bool
 		return isDefineDefined(define) && getDefine(define) != '1';
 
-	public static function areAnyDefinesDefined(defines:Array<ExprOf<String>>):Bool
+	public static macro function areAnyDefinesDefined(defines:Array<ExprOf<String>>):Bool
 	{
 		for (define in defines)
 			if (isDefineDefined(define))
@@ -26,11 +26,11 @@ class DefineManager
 		return false;
 	}
 
-	public static function runIfDefineDefined(define:ExprOf<String>, toRun:Dynamic->Void)
+	public static macro function runIfDefineDefined(define:ExprOf<String>, toRun:Dynamic->Void)
 		if (isDefineDefined(define) && toRun != null)
 			toRun(getDefine(define));
 
-	public static function runIfNotDefineDefined(define:ExprOf<String>, toRun:Dynamic->Void)
+	public static macro function runIfNotDefineDefined(define:ExprOf<String>, toRun:Dynamic->Void)
 		if (!isDefineDefined(define) && toRun != null)
 			toRun(getDefine(define));
 
